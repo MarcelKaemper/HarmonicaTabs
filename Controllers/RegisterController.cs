f9911c53e023b34e785b35f0d39f9e4ccecd1b26
MarcelKaemper <marcelkaemper862@gmail.com>
2019-11-09 19:34:27 +0100

    Added register form

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HarmonicaTabs.Models;
+
+namespace HarmonicaTabs.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Register()
+        {
+            var register = new Register();
+
+            return View(register);
+        }
+    }
+}
