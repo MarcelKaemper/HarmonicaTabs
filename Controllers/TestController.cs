1eefd2783537012ba0b83c9fcc737826a9ccfb2a
MarcelKaemper <marcelkaemper862@gmail.com>
2019-11-10 18:18:02 +0100

    Updated .gitignore

---
+++ Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HarmonicaTabs.Models;
+
+namespace HarmonicaTabs.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Test()
+        {
+            var test = new Test() { Id = 1 };
+
+            return View(test);
+        }
+    }
+}
---
+++ Models/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HarmonicaTabs.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public int type { get; set; }
+    }
+}
