83139b20e74016ef7764a11ee3a37907b515dfdb
MarcelKaemper <marcelkaemper862@gmail.com>
2019-11-12 21:24:44 +0100

    Function to handle DB connections

---
+++ Helpers/DatabaseConnector.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace HarmonicaTabs.Helpers
+{
+    public class DatabaseConnector
+    {
+
+        public MySqlConnection connection { get; set; }
+
+        public void openConnection()
+        {
+            connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString);
+            connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+    }
+}
