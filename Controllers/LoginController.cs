d75fa706040167df9487cd562dbd74324af762d1
MarcelKaemper <marcelkaemper862@gmail.com>
2019-11-12 20:56:43 +0100

    Basic login function

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HarmonicaTabs.Models;
+using MySql.Data.MySqlClient;
+
+namespace HarmonicaTabs.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private MySqlConnection connection;
+        private string errorMsg;
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View(new Login());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string login, string password)
+        {
+
+            string loginType = "username";
+
+            try
+            {
+                openConnection();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = connection;
+                //cmd.CommandText = string.Format(sql, generateUUID(), email, username, BCrypt.Net.BCrypt.HashPassword(password, 10));
+                cmd.CommandText = string.Format("SELECT password FROM logins WHERE {0}='" + login + "';", loginType);
+
+                MySqlDataReader reader;
+                reader = cmd.ExecuteReader();
+
+                reader.Read();
+                if (BCrypt.Net.BCrypt.Verify(password, reader.GetString(0)))
+                {
+                    errorMsg = "Login successful";
+                }
+                else
+                {
+                    errorMsg = "Login failed";
+                }
+
+                closeConnection();
+            }
+            catch (MySqlException e)
+            {
+                errorMsg = e.Message;
+            }
+
+            return View(new Login() { error = errorMsg });
+        }
+        private void openConnection()
+        {
+            connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["MySQLConnection"].ConnectionString);
+            connection.Open();
+        }
+
+        private void closeConnection()
+        {
+            connection.Close();
+        }
+    }
+}
